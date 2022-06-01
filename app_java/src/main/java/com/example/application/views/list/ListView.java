3bc03815c4b045be0320980d8ac4e4efbdd2f774
Konstantin Khomutov <khomutov.constantin@gmail.com>
2022-06-01 21:55:32 +0300

    update application with unit test

--- app_java/src/main/java/com/example/application/views/list/ListView.java
+++ app_java/src/main/java/com/example/application/views/list/ListView.java
@@ -1,5 +1,6 @@
 package com.example.application.views.list;
 
+import com.example.application.services.DateService;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
@@ -7,21 +8,15 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-
 @PageTitle("Hello DevOps")
 @Route(value = "")
 public class ListView extends VerticalLayout {
+    DateService dateService;
 
-    public ListView() {
+    public ListView(DateService dateService) {
         setSpacing(false);
-        String currentTimeString = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-        String moscowTime = LocalDateTime.now(ZoneId.of("Europe/Moscow"))
-                .format(DateTimeFormatter.ofPattern("d.MM.yyyy HH:mm 'hr'"));
+        String currentTimeString = dateService.getCurrentTimeString();
+        String moscowTime = dateService.getMoscowLocalTime();
 
         add(new H2("Current local date and time  " + currentTimeString));
         add(new H2("Moscow Time Zone date and time is " + moscowTime));
@@ -36,6 +31,8 @@ public class ListView extends VerticalLayout {
         setJustifyContentMode(JustifyContentMode.CENTER);
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         getStyle().set("text-align", "center");
+
+        this.dateService = dateService;
     }
 
 }

579331e49b8930ea36cbe8c98e070d38164ed45d
Konstantin Khomutov <khomutov.constantin@gmail.com>
2022-06-06 16:33:19 +0300

    add docker resource to see changes

---
+++ app_java/terraform/github/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = ">= 2.13.0"
+    }
+  }
+}
+resource "docker_image" "devops-labs" {
+  name = "konstantinkhomutov/devops-labs:latest"
+}
+
+resource "docker_container" "devops-labs-container" {
+  image = docker_image.devops-labs.latest
+  name  = "java-app"
+  ports {
+    internal = 8080
+    external = 8080
+  }
+}

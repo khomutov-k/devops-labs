f6fd1228127d03a0909acb1fe25baea51b511057
Konstantin Khomutov <khomutov.constantin@gmail.com>
2022-07-06 17:56:36 +0300

    lab 12 fixing file write

--- app_java/src/main/java/com/example/application/services/FileWritingService.java
+++ app_java/src/main/java/com/example/application/services/FileWritingService.java
@@ -3,7 +3,6 @@ package com.example.application.services;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,11 +13,13 @@ import java.nio.file.Paths;
 public class FileWritingService {
     private static String FILE_PATH = "./persist/data";
     static Path logFilePath = Paths.get(FILE_PATH);
+
     static {
         try {
             Files.createDirectories(logFilePath.getParent());
-            if( !Files.exists(logFilePath))
-                Files.createFile(Path.of(logFilePath.toFile() + ".txt"));
+            if (!Files.exists(logFilePath))
+                logFilePath = Path.of(logFilePath.toFile() + ".txt");
+            Files.createFile(logFilePath);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -26,8 +27,8 @@ public class FileWritingService {
 
     public void writeToFile(String str) throws IOException {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(logFilePath.toFile(), true))) {
-            writer.append("/n");
             writer.append(str);
+            writer.append("\n");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
